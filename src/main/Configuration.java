6daaf4c30fc6d6779334f106e7b85672341b6fff
Philipp Seitz <seitzphilipp22@gmail.com>
2018-02-26 18:43:18 +0100

    changed Math.random to the ultimate MersenneTwister

--- src/main/Configuration.java
+++ src/main/Configuration.java
@@ -1,8 +1,10 @@
 package main;
 
+import util.MersenneTwister;
 public enum  Configuration {
     instance;
     public int gridSize = 100;
     public double fireProbibility = 0.01;
+    public MersenneTwister random = new MersenneTwister();
 
 }
